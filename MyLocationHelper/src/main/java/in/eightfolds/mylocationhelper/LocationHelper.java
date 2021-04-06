479653235a8cd4b920de4f9b862f0ffdc3e38aff
Sanjay <sanjay@eightfolds.in>
2021-04-06 20:11:29 +0530

    first commit

---
+++ MyLocationHelper/src/main/java/in/eightfolds/mylocationhelper/LocationHelper.java
@@ -0,0 +1,10 @@
+package in.eightfolds.mylocationhelper;
+
+/**
+ * Created by Sanjay Singh on 4/6/2021.
+ */
+public class LocationHelper {
+    public static float getDistanceInKM(float distanceInMeter){
+        return distanceInMeter/1000;
+    }
+}
